ba8260e6fc465e3984c7b26ed5d0b63fc27d6d89
xsintill <xsintill@gmail.com>
2021-08-18 10:43:35 +0200

    added hover effects on movierow

--- apps/svelte-client/src/MovieList/MovieRow/MovieRow.svelte
+++ apps/svelte-client/src/MovieList/MovieRow/MovieRow.svelte
@@ -8,9 +8,17 @@
 <style lang="scss">
   div{  
     margin-bottom: 4px;
+    margin-left: 6px;
+    margin-right: 6px;
     padding-left: 16px;
     padding-top: 8px;
     padding-bottom: 8px;
     border-left: lightblue solid 4px;
+
+    &:hover {
+      background-color: rgba(243, 243, 243, 0.767);
+      box-shadow: 0 8px 16px rgba(0,0,0,0.25), 0 8px 8px rgba(0,0,0,0.22);
+      transform: scale(1.01);
+    }
   }
 </style>
