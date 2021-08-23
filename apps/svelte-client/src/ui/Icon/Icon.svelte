323c54655ba72a0228a611a757ec4c403b79cee7
xsintill <xsintill@gmail.com>
2021-08-24 00:13:02 +0200

    add Icon to the ui

---
+++ apps/svelte-client/src/ui/Icon/Icon.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    export let name;
+    export let width = "32";
+    export let height = "32";
+    export let fill = "gray";
+    export let viewBox = `0 0 ${width} ${height}`;
+    // export let focusable = false;
+
+
+</script>
+<svg 
+  width={width} 
+  height={height} 
+  fill={fill} 
+  viewBox={viewBox}
+  xmlns="http://www.w3.org/2000/svg">
+    <use href={`/assets/svgs/${name}.svg#${name}`}/>     
+</svg>
+
+<style lang="scss">   
+</style>
+
