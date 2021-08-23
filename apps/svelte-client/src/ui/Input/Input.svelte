a41e9ab7d688490243c23f576b1479a2fd30c55a
xsintill <xsintill@gmail.com>
2021-08-24 00:13:22 +0200

    add Input to the ui

---
+++ apps/svelte-client/src/ui/Input/Input.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    export let label: string;
+    export let id: string;
+    export let type: string = "text";
+</script>
+
+{#if label}
+<label for={id||label}>{label}</label>
+{/if}
+<input id={id||label} {type} {...$$props}/>
