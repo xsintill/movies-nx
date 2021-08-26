ea2c7c159e79783d7d0e80b75f09b1dd1411a958
xsintill <xsintill@gmail.com>
2021-08-26 12:00:36 +0200

    added searchMovie and getMovie Tmdb util

--- apps/svelte-client/src/tmdb/TMDB.utils.ts
+++ apps/svelte-client/src/tmdb/TMDB.utils.ts
@@ -1,11 +1,20 @@
+import { dataset_dev } from 'svelte/internal';
 import { ax } from '../axios/axios';
 import { environment } from '../environments/environment';
-import type { TMDBMovie } from './TMDB.type';
+import type { TMDBMovie, TMDBSearchMovieResult } from './TMDB.type';
 
 const url = 'https://api.themoviedb.org/3/';
 
 export async function getMovieByImdbId(imdbId: string): Promise<TMDBMovie> {
-  return await (
-    await ax.get(`${url}find/${imdbId}?api_key=${environment.tmdbApiKey}&external_source=imdb_id`)
-  ).data.movie_results[0];
+  return (await ax.get(`${url}find/${imdbId}?api_key=${environment.tmdbApiKey}&external_source=imdb_id`))
+    .data.movie_results[0];
+}
+
+export async function searchMovie(title: string): Promise<TMDBSearchMovieResult> {
+  return (await ax.get(`${url}search/movie?api_key=${environment.tmdbApiKey}&query=${title}&language=en-US&page=1&include_adult=true`))
+    .data.results[0];
+}
+
+export async function getMovie(tmdbId: number): Promise<TMDBMovie> {
+  return (await ax.get(`${url}movie/${tmdbId}?api_key=${environment.tmdbApiKey}`)).data;
 }
