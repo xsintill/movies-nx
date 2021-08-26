952c08c29e52c97972e9b43901df3427f5b6e82c
xsintill <xsintill@gmail.com>
2021-08-26 09:49:11 +0200

    -added postersize constants(other sizes are not supported by tmdb) and styling added -added reactive variable posterPath to deal with src or size changes directly

--- apps/svelte-client/src/MovieList/MoviePoster/MoviePoster.svelte
+++ apps/svelte-client/src/MovieList/MoviePoster/MoviePoster.svelte
@@ -1,14 +1,35 @@
 <script lang="ts">
+  import { TMDBPosterSize } from "../../tmdb/TMDB.const";
+
   export let src: string;
   export let alt: string;
+  export let size: string = TMDBPosterSize.w154;
+  let widthClass = size
 
-  const posterPath = src ? `http://image.tmdb.org/t/p/w154/${src}`: '/assets/images/placeholder-movie-poster.jpg'; 
+  $: posterPath = src ? `http://image.tmdb.org/t/p/${size}/${src}`: '/assets/images/placeholder-movie-poster.jpg'; 
 </script>
 
-<img src={posterPath} alt={alt}>
+<img {...$$props} class={`${$$props.class || ''} ${widthClass}`} src={posterPath} alt={alt} >
+
 
 <style lang="scss">
-  img {
+  .w92 {
+    width: 92px;
+  }
+  .w154 {
     width: 154px;
   }
+  .w185 {
+    width: 185px;
+  }
+  .w342 {
+    width: 342px;
+  }
+  .w500 {
+    width: 500px;
+  }
+  .w780 {
+    width: 780px;
+  }
+
 </style>
---
+++ apps/svelte-client/src/tmdb/TMDB.const.ts
@@ -0,0 +1,9 @@
+export const TMDBPosterSize = {
+  w92: "w92",
+  w154: "w154",
+  w185: "w185",
+  w342: "w342",
+  w500: "w500",
+  w780: "w780",
+  original: "original"
+}
