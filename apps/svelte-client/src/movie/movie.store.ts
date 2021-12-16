e094b623cb6f6738e0cc2a4328a2aab3bb47ef65
xsintill <xsintill@gmail.com>
2021-12-16 22:57:41 +0100

    lettercombos as a store

--- apps/svelte-client/src/movie/movie.store.ts
+++ apps/svelte-client/src/movie/movie.store.ts
@@ -4,3 +4,5 @@ import type { Movie } from './movie.type';
 
 export const latestMovies = writable<Movie[]>([]);
 export const tmdbMovies = writable<Movie[]>([]);
+export const combinedTmdbMovies = writable<Movie[]>([]);
+export const letterCombos = writable<string[]>([]);
