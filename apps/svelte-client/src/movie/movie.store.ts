153b1e856d5854569c6de8235e8a54548cbaeeab
xsintill <xsintill@gmail.com>
2022-01-02 09:06:07 +0100

    add some store values so we can determine whether we are retrieving movies or if the load more button can be shown

--- apps/svelte-client/src/movie/movie.store.ts
+++ apps/svelte-client/src/movie/movie.store.ts
@@ -6,3 +6,6 @@ export const latestMovies = writable<Movie[]>([]);
 export const tmdbMovies = writable<Movie[]>([]);
 export const combinedTmdbMovies = writable<Movie[]>([]);
 export const letterCombos = writable<string[]>([]);
+export const retrievingMovies = writable<boolean>(false);
+export const canLoadMore = writable<boolean>(true);
+export const showLoadMore = writable<boolean>(false);
